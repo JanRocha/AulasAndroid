8b00ea7490a4dd46a6e1c01942cf3f69a63f6534
Your Name <you@example.com>
2018-08-20 20:21:03 -0300

    teste

---
+++ curriculo2/app/src/main/java/br/com/uninove/curriculo2/DadosPessoaisActivity.java
@@ -0,0 +1,13 @@
+package br.com.uninove.curriculo2;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DadosPessoaisActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dados_pessoais);
+    }
+}
