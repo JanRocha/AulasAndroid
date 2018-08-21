6066e4b178b03a554afa789dcaabeb01fde65104
Your Name <you@example.com>
2018-08-20 21:52:56 -0300

    teste

--- curriculo2/app/src/main/java/br/com/uninove/curriculo2/MainActivity.java
+++ curriculo2/app/src/main/java/br/com/uninove/curriculo2/MainActivity.java
@@ -1,13 +1,30 @@
 package br.com.uninove.curriculo2;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity extends Activity {
-
+    Button btnDadosPessoais;
+    Button btnDadosProfissionais;
+    Button btnContato;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnDadosPessoais= (Button) findViewById(R.id.btnDadosPessoais);
+        btnContato=(Button) findViewById(R.id.btnContato);
+        btnDadosProfissionais=(Button) findViewById(R.id.btnDadosprofissionais);
+        //chamando a segunda tela
+
+    }
+    public void AbrirDadosPessoais(View view){
+        //Intentity -> inteção de fazer algo
+        Intent it = new Intent(this, DadosPessoaisActivity.class);
+        startActivity(it);
     }
 }
